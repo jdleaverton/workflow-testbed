3619bda95fa4bb5f35491499233ab04763482b38
jdleaverton <jdleaverton@gmail.com>
2023-09-07 13:45:04 -0500

    pass in args to DoPE.py

--- .github/workflows/DoPE.py
+++ .github/workflows/DoPE.py
@@ -1,2 +1,16 @@
+import sys
 import openai
-print("Success!")
+
+def main(modified, added, deleted, renamed):
+    print("Modified files:", modified)
+    print("Added files:", added)
+    print("Deleted files:", deleted)
+    print("Renamed files:", renamed)
+    print("Success!")
+
+if __name__ == "__main__":
+    modified = sys.argv[1].split()
+    added = sys.argv[2].split()
+    deleted = sys.argv[3].split()
+    renamed = sys.argv[4].split()
+    main(modified, added, deleted, renamed)
